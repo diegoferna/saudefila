01a79a484971a8da8347c2fd8bfd8ebfd0016f7d
diegoferna <diego.ferna@hotmail.com>
2024-03-13 15:31:39 -0300

    mudança na estrutura

--- api/src/routes/eventos.ts
+++ api/src/routes/eventos.ts
@@ -8,17 +8,19 @@ export async function eventoRoutes(app: FastifyInstance) {
         const createEndereco = z.object({
             nome: z.string(),
             local: z.string(),
-            data: z.string(),
+            horaInicio: z.string(),
+            horaFim: z.string(),
             status: z.string(),
         });
 
-        const { nome, local, data, status } = createEndereco.parse(request.body)
+        const { nome, local, horaInicio, horaFim, status } = createEndereco.parse(request.body)
 
         const evento = {
             id: randomUUID(),
             nome,
             local, 
-            data,
+            horaInicio,
+            horaFim,
             status
         }
 
